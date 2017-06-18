ae7b5293d12c57bb8ea8a67d82ab1c723d73fbcb
Berk Çalık <berkcancalik@gmail.com>
2017-06-18 17:14:32 +0300

    Add.

---
+++ CSharpFleckExample/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace CSharpFleckExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtServerAddress = new System.Windows.Forms.TextBox();
+            this.txtServerPort = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rtxtLog = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbClients = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtServerAddress
+            // 
+            this.txtServerAddress.Location = new System.Drawing.Point(12, 44);
+            this.txtServerAddress.Name = "txtServerAddress";
+            this.txtServerAddress.Size = new System.Drawing.Size(153, 22);
+            this.txtServerAddress.TabIndex = 0;
+            this.txtServerAddress.Text = "192.168.1.26";
+            // 
+            // txtServerPort
+            // 
+            this.txtServerPort.Location = new System.Drawing.Point(171, 44);
+            this.txtServerPort.Name = "txtServerPort";
+            this.txtServerPort.Size = new System.Drawing.Size(89, 22);
+            this.txtServerPort.TabIndex = 1;
+            this.txtServerPort.Text = "8181";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(13, 73);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(247, 32);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Server IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(178, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Port";
+            // 
+            // rtxtLog
+            // 
+            this.rtxtLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtxtLog.Location = new System.Drawing.Point(280, 73);
+            this.rtxtLog.Name = "rtxtLog";
+            this.rtxtLog.Size = new System.Drawing.Size(490, 390);
+            this.rtxtLog.TabIndex = 6;
+            this.rtxtLog.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(283, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Logs";
+            // 
+            // lbClients
+            // 
+            this.lbClients.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.lbClients.FormattingEnabled = true;
+            this.lbClients.ItemHeight = 16;
+            this.lbClients.Location = new System.Drawing.Point(15, 159);
+            this.lbClients.Name = "lbClients";
+            this.lbClients.Size = new System.Drawing.Size(245, 340);
+            this.lbClients.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Clients";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtMessage.Location = new System.Drawing.Point(354, 474);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(290, 24);
+            this.txtMessage.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(283, 477);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Message";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnSend.Location = new System.Drawing.Point(650, 471);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 28);
+            this.btnSend.TabIndex = 12;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 509);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbClients);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.rtxtLog);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtServerPort);
+            this.Controls.Add(this.txtServerAddress);
+            this.Name = "Form1";
+            this.Text = "Fleck WebSocket Server Example";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtServerAddress;
+        private System.Windows.Forms.TextBox txtServerPort;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox rtxtLog;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lbClients;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ CSharpFleckExample/Form1.cs
@@ -0,0 +1,90 @@
+﻿using Fleck;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpFleckExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        WebSocketServer server;
+        List<Fleck.IWebSocketConnection> clients = new List<IWebSocketConnection>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            var server = new WebSocketServer("ws://"+txtServerAddress.Text+":"+txtServerPort.Text);
+            server.Start(socket =>
+            {
+                socket.OnOpen = () => {
+                    log("Bağlantı sağlandı. : "+socket.ConnectionInfo.Id);
+                    clients.Add(socket);
+                    updateListBox();
+                };
+
+                socket.OnClose = () => {
+                    log("Bağlantı koptu. : " + socket.ConnectionInfo.Id);
+                    clients.Remove(socket);
+                    updateListBox();
+                }; 
+
+                socket.OnMessage = message =>
+                {
+                    log(message);
+                };  
+            });
+
+            log("Server başlatıldı.");
+            btnStart.Enabled = false; 
+        }
+
+        void log(string msg)
+        {
+            if (InvokeRequired)
+            {
+                Invoke((MethodInvoker)delegate { rtxtLog.Text += msg + "\r\n"; });
+                return;
+            }
+            else
+            {
+                rtxtLog.Text += msg + "\r\n";
+            }
+        }
+
+       void updateListBox()
+       {
+            Invoke((MethodInvoker)delegate {
+                lbClients.Items.Clear();
+                foreach (var item in clients)
+                {
+                    lbClients.Items.Add(item.ConnectionInfo.Id);
+                }
+            }); 
+       }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            foreach (var item in clients)
+            {
+                item.Send("_SERVER_MESSAGE_:"+txtMessage.Text);
+            }
+            log("_SERVER_MESSAGE_:" + txtMessage.Text);
+            txtMessage.Text = "";
+        }
+         
+    }
+}
